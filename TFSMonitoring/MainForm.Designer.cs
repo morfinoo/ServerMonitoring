3bf95cc27749964f950e09eefbe2ba3d24e24db9
Andrzej Juszkiewicz <andrzej.juszkiewicz@gmail.com>
2018-03-25 13:59:07 +0200

    Add project files.

---
+++ TFSMonitoring/MainForm.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace TFSMonitoring
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "TFS Monitoring";
+            this.notifyIcon1.BalloonTipTitle = "TFS Monitoring";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "TFS Monitoring";
+            this.notifyIcon1.Visible = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 72);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Interval [10-1800 sec]:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(188, 73);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(2);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1800,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(90, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(117, 103);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 19);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(10, 40);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Address:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(87, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(191, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 134);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "mainForm";
+            this.Text = "Server Monitoring";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.mainForm_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ TFSMonitoring/MainForm.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Reflection;
+using log4net;
+
+namespace TFSMonitoring
+{
+    public partial class mainForm : Form
+    {
+        Ping pingSender = new Ping();
+        PingOptions options = new PingOptions();
+        Thread thr;
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        private ContextMenu contextMenu1;
+        private MenuItem menuItem1;
+        private MenuItem menuItem2;
+        private string currentIP = "";
+        int interval = 60000;
+
+        public mainForm()
+        {
+            InitializeComponent();
+            contextMenu1 = new ContextMenu();
+            menuItem1 = new MenuItem();
+            menuItem1.Index = 0;
+            menuItem1.Text = "Exit";
+            menuItem1.Click += new EventHandler(menuItem1_Click);
+            menuItem2 = new MenuItem();
+            menuItem2.Index = 0;
+            menuItem2.Text = "Stop";
+            menuItem2.Click += new EventHandler(menuItem2_Click);
+            contextMenu1.MenuItems.AddRange(
+            new MenuItem[] { menuItem2, menuItem1 });
+            notifyIcon1.ContextMenu = contextMenu1;
+            log.Info("Application opened");
+            numericUpDown1.Value = interval / 1000;
+        }
+
+        private void mainLoop()
+        {
+            showBalloon("Start", "Server Monitoring started");
+            log.Info("System started");
+            log.Info("Interval set to " + (interval/1000) + " sec");
+
+            PingReply rply;
+            string tmpIP;
+
+            while (true)
+            {
+                try
+                {
+                    rply = runPing();
+                    if (rply.Status == IPStatus.Success)
+                    {
+                        tmpIP = rply.Address.ToString();
+                        if (tmpIP != currentIP)
+                        {
+                            currentIP = tmpIP;
+                            showBalloon("Change!", "Server IP Changed to " + currentIP);
+                            log.Info("Server IP Changed to " + currentIP);
+                            notifyIcon1.Text = "Server Monitoring\nIP address: " + currentIP + "";
+                        }
+                    }
+                    else
+                    {
+                        tmpIP = "no connection";
+                        if (tmpIP != currentIP)
+                        {
+                            currentIP = tmpIP;
+                            showBalloon("NO CONNECTION!", "Connection to server lost");
+                            log.Warn("Connection to server lost");
+                            notifyIcon1.Text = "Server Monitoring\nNo Connection!!";
+                        }
+                        Thread.Sleep(3000);
+                        continue;
+                    }
+                }
+                catch
+                {
+                    tmpIP = "no connection";
+                    if (tmpIP != currentIP)
+                    {
+                        currentIP = tmpIP;
+                        showBalloon("NO CONNECTION!", "Connection to server lost");
+                        log.Warn("Connection to server lost");
+                        notifyIcon1.Text = "Server Monitoring\nNo Connection!!";
+                    }
+                    Thread.Sleep(3000);
+                    continue;
+                }
+                Thread.Sleep(interval);
+            }
+        }
+        private PingReply runPing()
+        {
+            options.DontFragment = true;
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            int timeout = 4000;
+            PingReply reply = pingSender.Send(textBox1.Text, timeout, buffer, options);
+            return reply;
+        }
+
+        private void menuItem1_Click(object Sender, EventArgs e)
+        {
+            thr.Abort();
+            log.Info("System stopped");
+            Application.Exit();
+        }
+
+        private void menuItem2_Click(object Sender, EventArgs e)
+        {
+            thr.Abort();
+            log.Info("System stopped");
+            Show();
+            notifyIcon1.Visible = false;
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void showBalloon(string title, string body)
+        {
+            string titTxt = "", bdyTxt = "";
+            if (title != null) titTxt = title;
+            if (body != null) bdyTxt = body;
+            notifyIcon1.ShowBalloonTip(4000, titTxt, bdyTxt, ToolTipIcon.Info);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            currentIP = "";
+            thr = new Thread(mainLoop);
+            thr.Start();
+            Hide();
+            notifyIcon1.Visible = true;
+        }
+
+        private void mainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            log.Info("Application closed");
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            interval = (int)numericUpDown1.Value * 1000;
+        }
+    }
+}
